2fa24cc43a836cb0dce06cb83cd6f49a57a54f72
jhetzel <Julius.Hetzel@gmx.de>
2018-02-22 11:59:01 +0100

    getConfig added to environment

--- src/main/kotlin/de/jhetzel/mvp/Environment.kt
+++ src/main/kotlin/de/jhetzel/mvp/Environment.kt
@@ -2,6 +2,7 @@ package de.jhetzel.mvp
 
 import de.jhetzel.mvp.eventbus.EventBus
 import de.jhetzel.mvp.concurrency.Executor
+import kotlin.reflect.KClass
 
 
 interface Environment {
@@ -9,4 +10,8 @@ interface Environment {
     fun getEventBus(): EventBus
 
     fun getOperationExecutor(): Executor
+
+    fun <T : Any> getConfig(configClass: KClass<T>)
 }
+
+inline fun <reified T : Any> Environment.getConfig() =  getConfig(T::class)
